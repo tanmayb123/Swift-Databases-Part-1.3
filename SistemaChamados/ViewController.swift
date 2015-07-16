5fda9bdabef7e3a21b9f7ddf49ecdbbdaf16385e
Luan Felix Coelho <luanfelix@Luans-Mac.local>
2015-07-16 16:00:50 -0300

    Initial Commit

---
+++ SistemaChamados/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SistemaChamados
+//
+//  Created by Luan Felix Coelho on 7/16/15.
+//  Copyright (c) 2015 Luan Felix Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
